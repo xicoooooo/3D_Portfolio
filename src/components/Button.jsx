b8159c7240c7eece9425604c0d27a171fb997b86
xico <faragaodias@gmail.com>
2025-05-27 16:30:52 +0100

    refactor: update scroll logic in Button component for improved functionality

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -12,14 +12,13 @@ const Button = ({ text, className, id }) => {
 
         const target = document.getElementById("counter"); // Find the section with ID "counter"
 
-        // Only scroll if we found the section and an ID is passed in
-        // taht prevents the contact button from scrolling to the top
+        // Only scroll if we found the section and an ID is passed in that prevents the contact button from scrolling to the top
         if (target && id) {
           const offset = window.innerHeight * 0.15; // Leave a bit of space at the top
 
           // Calculate how far down the page we need to scroll
           const top =
-            target.getBoundingClientRect().top + window.pageYOffset - offset;
+            target.getBoundingClientRect().top + window.scrollY - offset;
 
           // Scroll smoothly to that position
           window.scrollTo({ top, behavior: "smooth" });
